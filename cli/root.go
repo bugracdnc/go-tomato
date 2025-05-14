fc54e50583a7b816d008691830d1ffea4b03b4be
bugracdnc <bugracaliskandinc@gmail.com>
2025-05-14 19:00:04 +0300

    Changed flag structure

--- cli/root.go
+++ cli/root.go
@@ -30,10 +30,10 @@ func Execute() {
 }
 
 func init() {
-	rootCmd.Flags().IntVarP(&pomodoro.StudyDuration, "study", "s", 25, "Set study duration in minutes")
+	rootCmd.Flags().IntVarP(&pomodoro.StudyDuration, "duration", "d", 25, "Set study duration in minutes")
 	rootCmd.Flags().IntVarP(&pomodoro.BreakDuration, "break", "b", 5, "Set break duration in minutes")
-	rootCmd.Flags().IntVarP(&pomodoro.LongBreakIntervals, "long-break-intervals", "i", long_break_interval_default, "Set intervals for long break")
-	rootCmd.Flags().IntVarP(&pomodoro.LongBreakDuration, "long-break-duration", "l", long_break_duration_default, "Set long break duration")
-	rootCmd.Flags().BoolVarP(&pomodoro.DoLongBreaks, "enable-long-breaks", "e", false, "Enable long breaks")
+	rootCmd.Flags().IntVar(&pomodoro.LongBreakIntervals, "long-break-intervals", long_break_interval_default, "Set intervals for long break")
+	rootCmd.Flags().IntVar(&pomodoro.LongBreakDuration, "long-break-duration", long_break_duration_default, "Set long break duration")
+	rootCmd.Flags().BoolVar(&pomodoro.DoLongBreaks, "disable-long-breaks", true, "Disable long breaks")
 	rootCmd.Flags().StringVarP(&pomodoro.Title, "title", "t", "tomato", "Set a title")
 }
