20d62a58b5bb1a3ff0589afd63f08ec9fefbb837
bugracdnc <bugracaliskandinc@gmail.com>
2025-05-15 03:07:47 +0300

    Moved input handling to pomodoro from timer to detect consistently between timers

--- timer/timer.go
+++ timer/timer.go
@@ -2,66 +2,55 @@ package timer
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
-	"os/signal"
 	"syscall"
 	"time"
 )
 
-// A string const filled with space to clear the line before printing the remaining time
-const clear_line = "                                          "
+// Enum type for return values of the timer
+type TimerReturn int
+
+// Timer can either finish by itself or can be skipped, represent these two states
+const (
+	TimerDone TimerReturn = iota
+	TimerSkipped
+)
 
 // Start a timer for given duration
-func TimerFor(duration time.Duration) int {
+func TimerFor(duration time.Duration, ch chan byte) TimerReturn {
 	// Calculate the end time by adding the duration to time.Now()
 	end := time.Now().Add(time.Minute * duration)
-
-	//Here we use go routines to get input from the user during the timer
-	ch := make(chan byte)
-	go func(ch chan byte) {
-		// Disable input buffering
-		exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
-		// Do not display entered characters on the screen
-		exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
-
-		// Catch interrupt signal (^C) to be able to set the terminal back to sane
-		sigs := make(chan os.Signal, 1)
-		signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
-		// Execute if interrupt signal is detected
-		go func() {
-			<-sigs
-			// Set terminal back to sane
-			exec.Command("stty", "-F", "/dev/tty", "sane").Run()
-			// Manually print the signal: interrupt message
-			fmt.Println("signal: interrupt")
-			// Exit gracefully
-			os.Exit(0)
-		}()
-
-		// byte array with a length of 1 to capture user input
-		var b []byte = make([]byte, 1)
-		for {
-			os.Stdin.Read(b) // capture user input
-			ch <- b[0]       // suppy the channel
-		}
-	}(ch)
 
 	for {
 		// If a key is pressed, detect it, else pass on
 		select {
 		case stdin := <-ch:
-			fmt.Printf("        %q", stdin)
+			switch stdin {
+			case 'q':
+				// Exit by sending an interrupt signal to be detected by the input listener in pomodoro and exit from there
+				syscall.Kill(syscall.Getpid(), syscall.SIGQUIT)
+			case 's':
+				// Skip to the next timer by ending the current one
+				fmt.Printf("\r\nSkipping session...\r\n")
+				return TimerSkipped
+			case '+':
+				// Add a minute to the timer
+				end = end.Add(time.Minute)
+			case '-':
+				// Remove a minute from the timer
+				if end.Minute() > 1 {
+					end = end.Add(-time.Minute)
+				}
+			}
 		default:
 		}
 
 		// If the time now is after the end, return gracefully (i.e., detect if the timer should stop)
 		if time.Now().After(end) {
-			return 0
+			return TimerDone
 		}
 		// Else, calculate the remaining time and print formatted
 		remaining := time.Time{}.Add(end.Sub(time.Now()))
-		fmt.Printf("\r%s\r%s ", clear_line, remaining.Format("04:05")) // Clear the line with the empty line string and print back over with the time
-		time.Sleep(1000)                                               // Wait for a second since the remaining time output will not change between seconds
+		fmt.Printf("\r     \r%s ", remaining.Format("04:05")) // Clear the line with the empty line string and print back over with the time
+		time.Sleep(1000)                                      // Wait for a second since the remaining time output will not change between seconds
 	}
 }
--- tomato/pomodoro.go
+++ tomato/pomodoro.go
@@ -3,8 +3,62 @@ package tomato
 import (
 	"fmt"
 	"go-tomato/timer"
+	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+func disableInputBuf() {
+	// Disable input buffering
+	exec.Command("stty", "-F", "/dev/tty", "cbreak", "min", "1").Run()
+	// Do not display entered characters on the screen
+	exec.Command("stty", "-F", "/dev/tty", "-echo").Run()
+}
+
+func enableInputBuf() {
+	// Set terminal back to sane
+	exec.Command("stty", "-F", "/dev/tty", "sane").Run()
+}
+
+func startInputListener(ch chan byte) {
+	//Function to disable input buffering and disable echo
+	disableInputBuf()
+
+	// Catch interrupt signal (^C) to be able to set the terminal back to sane
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	// Execute if interrupt signal is detected
+	go func() {
+		sig := <-sigs
+		// Set terminal back to sane
+		enableInputBuf()
+
+		// Exit normally if SIGQUIT
+		if sig == syscall.SIGQUIT {
+			fmt.Printf("\r\nExiting...\r\n")
+			os.Exit(0)
+		} else { // exit with the signal
+			fmt.Printf("signal: %s\r\n", sig.String())
+			os.Exit(128 + int(sig.(syscall.Signal)))
+		}
+	}()
+	go func() {
+		// byte array with a length of 1 to capture user input
+		var b []byte = make([]byte, 1)
+		for {
+			os.Stdin.Read(b) // capture user input
+			ch <- b[0]       // suppy the channel
+		}
+	}()
+
+}
+
+// Prints specific user inputs to interract with the program
+func printInputUsage() {
+	fmt.Printf("(s)kip, (+) add minute, (-) remove minute, (q)uit\r\n")
+}
 
 // Line to be put between complete whole sessions
 const line = "##############################"
@@ -19,13 +73,20 @@ type Pomodoro struct {
 
 // Start the timer initialized in cli/root.go by creating timers for study and breaks with the set values in an endless loop
 func (p *Pomodoro) StartTimer() {
+	// Declare a chan to capture key presses in a goroutine
+	ch := make(chan byte)
+
+	// Start the input listener
+	startInputListener(ch)
+
 	//i to count the sessions
 	i := 1
 	for {
 		// Print starting message
 		fmt.Printf("Starting %s #%d\n\r", p.Title, i)
+		printInputUsage()
 		// Start the study session timer
-		timer.TimerFor(time.Duration(p.StudyDuration))
+		timer.TimerFor(time.Duration(p.StudyDuration), ch)
 		// Indicate the end of study session
 		fmt.Printf("\n\rStudy session %s #%d ended!\r\n\n", p.Title, i)
 
@@ -35,8 +96,9 @@ func (p *Pomodoro) StartTimer() {
 
 			// Print long break start
 			fmt.Printf("Starting long break #%d\n\r", i/p.LongBreakIntervals)
+			printInputUsage()
 			// Start the long break timer
-			timer.TimerFor(time.Duration(p.LongBreakDuration))
+			timer.TimerFor(time.Duration(p.LongBreakDuration), ch)
 			// Indicate long break end
 			fmt.Printf("\n\rLong break session %s #%d ended!\r\n\n", p.Title, i/p.LongBreakIntervals)
 
@@ -44,8 +106,9 @@ func (p *Pomodoro) StartTimer() {
 
 			// Print break start
 			fmt.Printf("Starting break #%d\n\r", i)
+			printInputUsage()
 			// Start the break timer
-			timer.TimerFor(time.Duration(p.BreakDuration))
+			timer.TimerFor(time.Duration(p.BreakDuration), ch)
 			// Indicate break end
 			fmt.Printf("\n\rBreak session %s #%d ended!\r\n\n", p.Title, i)
 		}
